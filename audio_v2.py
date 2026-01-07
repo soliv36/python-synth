793bc1b0927dabd2410641b747fc965d5a70c604
M <111779592+mc-estrada@users.noreply.github.com>
2026-01-07 03:13:35 -0700

    testing changes
    added ode to joy intro test case and commented out the play subprocess call since afplay is mac specific

--- audio_v2.py
+++ audio_v2.py
@@ -309,9 +309,13 @@ def gen_waveform(notes:list):
 	return output
 
 # plays output waveform's sound
+# can only feed array of notes processed as a waveform, not an array of raw notes
 def play(output:np.array):
+	#folder = "python-audio"
+	filename = "output_v2.wav"
+	#fullpath = f'{folder}/{filename}'
 	write("output.wav", SAMPLERATE, output.astype(np.int16))
-	subprocess.run("afplay output.wav", shell=True)
+	#subprocess.run("afplay output.wav", shell=True)
 
 ## TEST
 a4 = Note('a4', adsr=[0.5, 0.2, 0.1, 0.1], effects=[('h', 4), ('v', 3, 'sine')])
@@ -323,6 +327,7 @@ g4 = Note('g4', adsr=[0.5, 0.2, 0.1, 0.1], effects=[('h', 4), ('v', 3, 'sine')])
 b4 = Note('b4', adsr=[0.5, 0.2, 0.1, 0.1], effects=[('h', 4), ('v', 3, 'sine')])
 d4 = Note('d4', adsr=[0.5, 0.2, 0.1, 0.1], effects=[('h', 4), ('v', 3, 'sine')])
 f4 = Note('f4', adsr=[0.5, 0.2, 0.1, 0.1], effects=[('h', 4), ('v', 3, 'sine')])
+f4s = Note('f4s', adsr=[0.5, 0.2, 0.1, 0.1],effects=[('h', 4), ('v', 3,'sine')])
 g5 = Note('g5', adsr=[0.5, 0.2, 0.1, 0.1], effects=[('h', 4), ('v', 3, 'sine')])
 c4 = Note('c4', adsr=[0.5, 0.2, 0.1, 0.1], effects=[('h', 4), ('v', 3, 'sine')])
 
@@ -341,7 +346,11 @@ notes_1 = [a_minor_1,
 		 a_minor_1,
 		 d_minor
 ]
-
+sector_1 = [f4s, f4s, g4, a4, a4, g4, f4s, e4 , d4 , d4 , e4 , f4s ,  f4s ,  e4 ,  e4]
+notes_2 = [*sector_1 , *sector_1]
 output_1 = gen_waveform(notes_1)
 output_1 = apply_filter(output_1, 'lowpass', [630], 2)
-play(output_1)
+output_2 = gen_waveform(notes_2)
+output_2 = apply_filter(output_2, 'lowpass', [630], 2)
+
+play(output_2)
